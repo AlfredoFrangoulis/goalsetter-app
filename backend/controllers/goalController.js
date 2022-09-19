bb2be1322056604d4fef66560b5c0980fe0001b2
Alfredo Frangoulis <alfredofrangoulis@gmail.com>
2022-09-19 11:07:37 +0700

    Added frontend UI

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 const getGoals = asyncHandler(
     async (req, res) => {
-        const goals = await Goal.find()
+        const goals = await Goal.find({ user: req.user.id })
         res.status(200).json(goals)
     }
 )
@@ -17,7 +18,8 @@ const setGoal = asyncHandler(
         }
 
         const goal = await Goal.create({
-            text: req.body.text
+            text: req.body.text,
+            user: req.user.id
         })
         res.status(200).json(goal)
     }
@@ -32,11 +34,24 @@ const updateGoal = asyncHandler(
             throw new Error('Goal not found')
         }
 
+        const user = await User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        if(goal.user.toString() !== user.id) {
+            res.status(401)
+            throw new Error('User not authorized')
+        }
+
         const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
         res.status(200).json(updatedGoal)
     }
 )
+
 const deleteGoal = asyncHandler(
     async (req, res) => {
         const goal = await Goal.findById(req.params.id)
@@ -44,6 +59,18 @@ const deleteGoal = asyncHandler(
         if(!goal) {
             res.status(400)
             throw new Error('Goal not found')
+        }
+
+        const user = await User.findById(req.user.id)
+
+        if(!user) {
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        if(goal.user.toString() !== user.id) {
+            res.status(401)
+            throw new Error('User not authorized')
         }
 
         await goal.remove()
